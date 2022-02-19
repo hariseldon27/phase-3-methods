b0a19d1ca2cbb417eff67252f64957ff1401f0ac
Jonathan Potter <jonathan@mrjonathanpotter.com>
2022-02-18 16:02:06 -0800

    commit tree c2ad5c0f25cc715223dcb61fa38a9acac93b4f81

--- methods.rb
+++ methods.rb
@@ -1,1 +1,34 @@
 # Your code here!
+def greet_programmer
+    puts "Hello, programmer!"
+end
+
+greet_programmer
+
+def greet(name)
+    puts "Hello, " + name + "!"
+end
+
+greet("Naureen")
+
+def greet_with_default(name = "programmer")
+    puts "Hello, #{name}!"
+end
+
+
+def add(num1,num2)
+    return num1 + num2
+end
+
+#puts add(1,2)
+
+def halve(number)
+    if number.class != Integer
+        puts "oh snap" 
+        return nil
+    end
+
+    return number / 2
+end
+
+puts halve(6)
